a10e43e0b69a15f09d128fc6a8327944996b1eb2
zhangkaimin <xiaoluo.260@163.com>
2020-04-02 20:44:40 +0800

    日志管理系统

---
+++ manage_glus_log/manage_glus_log_centos-x86/install.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+chmod +x *.sh
+./cron_manage_glus_log.sh
---
+++ manage_glus_log/manage_glus_log_ubuntu-arm/cron_manage_glus_log.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [ ! -f /etc/logrotate.d/glusterfs ]; then
+    \cp -f glusterfs /etc/logrotate.d/glusterfs
+fi
+if [ ! -f /etc/logrotate.d/glusterfs-georep ]; then
+    \cp -f glusterfs-georep /etc/logrotate.d/glusterfs-georep
+fi
+mkdir -p /opt/manage_glus_log
+\cp -f manage_glus_log.sh /opt/manage_glus_log/manage_glus_log.sh
+chmod +x /opt/manage_glus_log/manage_glus_log.sh
+echo "30 * * * * root /opt/manage_glus_log/manage_glus_log.sh"  >> /etc/crontab
+service cron start 
